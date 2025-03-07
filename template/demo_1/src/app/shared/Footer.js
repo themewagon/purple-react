67cc42c99d6dde898ceef2ee7f303059e75de6d3
AnikaBristy <bristy.technext@gmail.com>
2025-03-07 20:44:22 +0600

    Footer updated

--- template/demo_1/src/app/shared/Footer.js
+++ template/demo_1/src/app/shared/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
     return (
       <footer className="footer">
         <div className="d-sm-flex justify-content-center justify-content-sm-between py-2">
-          <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="https://www.bootstrapdash.com/" target="_blank" rel="noopener noreferrer">bootstrapdash.com </a>2020</span>
+          <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="https://www.bootstrapdash.com/" target="_blank" rel="noopener noreferrer">bootstrapdash.com </a>2020. Distributed by <a href="http://themewagon.com">ThemeWagon</a></span>
           <span className="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Free <a href="https://www.bootstrapdash.com/react-admin-templates/" target="_blank" rel="noopener noreferrer"> react admin </a> templates from BootstrapDash.com.  </span>
         </div>
       </footer> 
